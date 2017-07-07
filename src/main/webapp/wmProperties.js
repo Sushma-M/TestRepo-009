44166e721381402a6c6a711c3733c75a8537c3af
root <root@f3740970baf7>
2017-07-07 06:44:15 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestRepo 009",
+  "homePage" : "Main",
+  "name" : "TestRepo_009",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
